24bd2465ec73f65df84dd9fecc1b85e04593c8c2
danii <danii@DANIIL>
2025-12-06 23:00:49 +0500

    asd

---
+++ Tyuiu.MedvedevDP.Sprint5.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedvedevDP.Sprint5.Task5.V5.Lib
+{
+    public class DataService : ISprint5Task5V5
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            double maxInt = double.MinValue; 
+            double minInt = double.MaxValue; 
+            bool foundInt = false; 
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string number in numbers)
+                    {
+                        string numStr = number.Replace('.', ',');
+
+                        if (double.TryParse(numStr, out double value))
+                        {
+                            if (value % 1 == 0)
+                            {
+                                if (value > maxInt) maxInt = value;
+                                if (value < minInt) minInt = value;
+                                foundInt = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (foundInt)
+            {
+                res = maxInt - minInt;
+            }
+            else
+            {
+                res = 0;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint5.Task5.V5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Tyuiu.MedvedevDP.Sprint5.Task5.V5.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint5.Task5.V5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V5.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            if (File.Exists(path))
+            {
+                double res = ds.LoadFromDataFile(path);
+                Console.WriteLine("Разница между макс. и мин. целыми числами = " + res);
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден! Создайте файл " + path);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
